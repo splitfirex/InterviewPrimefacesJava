1650657cf29b567c215b668bc8dac3ff36cd71ec
Daniel Farina <dfarina@idom.com>
2020-05-13 15:06:06 +0200

    Primera version

--- backend/src/main/java/es/app/back/dao/ProductoDao.java
+++ backend/src/main/java/es/app/back/dao/ProductoDao.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
-@Repository
 public class ProductoDao extends AbstractCrudDao<Producto> {
 
     @Override
--- backend/src/main/java/es/app/back/services/ProductoServiceImpl.java
+++ backend/src/main/java/es/app/back/services/ProductoServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
-@Service
 public class ProductoServiceImpl implements ProductoService {
 
     @Autowired
